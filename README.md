4185c3709bbbdc88b661f03c31779696225a49ec
Gobbera <leonardo.lgobbo@gmail.com>
2023-03-10 16:36:56 -0300

    adddd

---
+++ README.md
@@ -0,0 +1,10 @@
+Adicionando o embbed :
+    Insira a tag "script" no "head" do seu arquivo HTML juntamente com o caminho do arquivo embbedChat.js:
+        "<script src="embbedChat/embbedChat.js"></script>"
+
+Configurações:
+    Para configurar a aparencia:
+        "/embbedChat/config/embbedChatStyle.css"   
+    
+    Para configurar funções de controle:
+        "/embbedChat/config/embbedChatController.css"
---
+++ embbedChat.js
@@ -0,0 +1,68 @@
+const id = document.currentScript.getAttribute('data-id');
+const embbedApp = document.currentScript.getAttribute('data-embbedApp');
+
+console.log(id);
+console.log(embbedApp);
+
+const embbedLink = document.getElementById('embbedLink');
+
+const embbedStyle = document.createElement('link');
+const embbedScript = document.createElement('script');
+
+const boxDiv = document.createElement('div');
+
+const iframe = document.createElement('iframe');
+
+const topBarDiv = document.createElement('div');
+
+const openChatButton = document.createElement('button');
+const openChatButtonImg = document.createElement('img');
+
+const closeChatButton = document.createElement('button');
+const closeButtonImg = document.createElement('img');
+
+function start() {
+    document.head.appendChild(embbedStyle);
+    embbedStyle.setAttribute('rel', 'stylesheet');
+    embbedStyle.setAttribute('href', '/config/embbedChatStyle.css');
+
+    document.body.appendChild(openChatButton);
+    openChatButton.setAttribute('id', 'chat-button');
+    openChatButtonImg.setAttribute('id', 'embbed-icons');
+    openChatButtonImg.setAttribute('src', '/embbed-icons/chat-icon.png');
+    openChatButtonImg.setAttribute('alt', 'chat-icon.png');
+    openChatButton.appendChild(openChatButtonImg);
+    openChatButton.setAttribute('class', 'rounded-button');
+    openChatButton.classList.add('chat-button');
+
+    document.body.appendChild(boxDiv);
+    boxDiv.setAttribute('class', 'box');
+    boxDiv.setAttribute('id', 'embbed-chat');
+    boxDiv.appendChild(topBarDiv);
+    boxDiv.appendChild(iframe);
+    
+    topBarDiv.setAttribute('class', 'top-chat-bar');
+
+    iframe.setAttribute('id', 'embbed-chat');
+    iframe.setAttribute('width', '100%');
+    iframe.setAttribute('height', '89.4%');
+    iframe.setAttribute('src', embbedLink.dataset.link);
+    iframe.setAttribute('frameborder', '0');
+
+    topBarDiv.appendChild(closeChatButton);
+
+    closeChatButton.setAttribute('id', 'close-button');
+    closeButtonImg.setAttribute('id', 'embbed-icons');
+    closeButtonImg.setAttribute('src', '/embbed-icons/close-icon.png');
+    closeButtonImg.setAttribute('alt', 'close-icon.png');
+    closeChatButton.appendChild(closeButtonImg);
+    closeChatButton.setAttribute('class', 'rounded-button');
+
+    document.body.appendChild(embbedScript);
+    embbedScript.setAttribute('src', '/config/embbedChatController.js');
+}
+
+window.addEventListener('load', start);
+
+
+
